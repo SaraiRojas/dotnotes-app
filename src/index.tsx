df212d0cea55693d223baefce359ae17835fffc5
Sarai Rojas <sarai.rojas@conekta.com>
2022-07-14 17:29:51 -0500

    feat: add login btn

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 // import App from './App';
 import reportWebVitals from './reportWebVitals';
-import LogIn from './views/LogIn';
+import LogIn from './views/LogIn/LogIn';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
---
+++ src/views/LogIn/LogIn.tsx
@@ -0,0 +1,17 @@
+import * as React from 'react'
+import Button from '@mui/material/Button';
+
+const LogIn = () => {
+  return (
+    <main>
+      <Button
+        variant="outlined"
+        style={{ borderRadius: '14px', borderWidth: '3px', color: '#000000', borderColor: '#000000' }}
+      >
+        Continue with Google
+      </Button>
+    </main>
+  )
+}
+
+export default LogIn
