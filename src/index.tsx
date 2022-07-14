e379f5e4e9d88c90d5d39c000d528ab24c482df2
Sarai Rojas <sarai.rojas@conekta.com>
2022-07-14 16:55:28 -0500

    chore: set proyect

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+// import App from './App';
 import reportWebVitals from './reportWebVitals';
+import LogIn from './views/LogIn';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <LogIn />
   </React.StrictMode>
 );
 
---
+++ src/views/LogIn.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react'
+
+const LogIn = () => {
+  return (
+    <main>
+      <Button variant="outlined" href="#outlined-buttons">
+        Link
+      </Button>
+    </main>
+  )
+}
+
+export default LogIn
