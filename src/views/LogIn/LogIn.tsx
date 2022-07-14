6eccf491a8a5e5ff4526ac53093514bf8a28bd3a
Sarai Rojas <sarai.rojas@conekta.com>
2022-07-14 18:19:44 -0500

    chore: set css modules

--- src/views/LogIn/LogIn.tsx
+++ src/views/LogIn/LogIn.tsx
@@ -1,9 +1,12 @@
 import * as React from 'react'
 import Button from '@mui/material/Button';
+import styles from "./LogIn.module.css"
 
 const LogIn = () => {
   return (
     <main>
+      <h1>DotNotes<span>.</span></h1>
+      <p className={styles.subtitle}>Start taking notes</p>
       <Button
         variant="outlined"
         style={{ borderRadius: '14px', borderWidth: '3px', color: '#000000', borderColor: '#000000' }}
