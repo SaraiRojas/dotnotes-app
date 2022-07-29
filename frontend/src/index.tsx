91fefa00e0e02796111aecd623a5c00a1a1e8700
Sarai Rojas <sarai.rojas@conekta.com>
2022-07-29 18:00:17 -0500

    feat: Add LogIn component

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+// import App from './App'
+import './index.scss'
+import LogIn from './views/LogIn/LogIn'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <LogIn />
   </React.StrictMode>
 )
