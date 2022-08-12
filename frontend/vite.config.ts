76e731f9ebbfa2040997e718b5a42caa125eb2c3
gsusAguirreArz <daniel.aguirre27@outlook.com>
2022-08-12 16:26:09 -0500

    changed two lines of compose file, and aaded extra config on vite config file

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,7 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
-    port: 5173
+    port: 5173,
+    host: true
   },
   plugins: [react()]
 })
