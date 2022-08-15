98692794cd5644fd71aba2ba38ebc930a724c835
Sarai Rojas <sarai.rojas@conekta.com>
2022-08-15 18:55:32 -0500

    feat: set Auth0

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-// import App from './App'
 import './scss/index.scss'
 import LogIn from './views/LogIn/LogIn'
+import { Auth0Provider } from "@auth0/auth0-react"
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <LogIn />
+    <Auth0Provider
+      domain={import.meta.env.VITE_AUTH0_DOMAIN}
+      clientId={import.meta.env.VITE_AUTH0_CLIENT_ID}
+      redirectUri={window.location.origin}
+    >
+      <LogIn />
+    </Auth0Provider>
   </React.StrictMode>
 )
