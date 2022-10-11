632b1a9fd02c87a813259b6a52d6a1755748ac53
Sarai Rojas <sarai.rojas@conekta.com>
2022-10-11 16:58:20 -0500

    feat: add actions to fullNote

--- frontend/src/components/FullNote/FullNote.tsx
+++ frontend/src/components/FullNote/FullNote.tsx
@@ -1,15 +1,28 @@
-import { Typography } from '@mui/material'
+import { Button, Typography } from '@mui/material'
 import { useContext } from 'react'
 import { AuthContext } from '../../context/AuthContextProvider'
 import '../../scss/index.scss'
 import { formatDateToString } from '../../utils/formaters'
 import NoteContainer from '../NoteContainer/NoteContainer'
+import ArrowBackIcon from '@mui/icons-material/ArrowBack'
+import MoreHorizIcon from '@mui/icons-material/MoreHoriz'
+import { useNavigate } from 'react-router-dom'
+import DropDownMenu from '../DropDownMenu/DropDownMenu'
 
 const FullNote = () => {
   const { noteInfo } = useContext(AuthContext)
 
+  const navigate = useNavigate()
+
   return (
     <NoteContainer cardHeight="100vh" cardWidth="90vw">
+      <div className="fullNote_actions">
+        <ArrowBackIcon
+          className="fullNote_icon"
+          onClick={() => navigate('/notes')}
+        />
+        <DropDownMenu Name={MoreHorizIcon} />
+      </div>
       <Typography className={'NoteContainer_title'}>
         {noteInfo.note.title}
       </Typography>
