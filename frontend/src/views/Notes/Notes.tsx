e087729172b201e92bf27e4bcc3f5500acada5ba
Sara-Rojas <sara.r.sandoval@gmail.com>
2023-01-31 15:02:53 -0600

    feat: create Add new note btn

--- frontend/src/views/Notes/Notes.tsx
+++ frontend/src/views/Notes/Notes.tsx
@@ -7,38 +7,49 @@ import { Stack } from '@mui/material'
 import { INote } from '../../model/interface'
 import { ReactComponent as Icon } from '../../assets/sad-face.svg'
 import _ from 'lodash'
+import SpeedDial from '@mui/material/SpeedDial'
+import SpeedDialIcon from '@mui/material/SpeedDialIcon'
 
 const Notes = () => {
   const { isAuthenticated, user } = useContext(AuthContext)
   console.log(isAuthenticated, user)
 
-  const [notes, setNotes] = useState<INote[]>(null)
+  const [notes, setNotes] = useState<INote[]>([])
 
   useEffect(() => {
     getNotes(user.sub)
       .then((data: any) => {
-        console.log(data)
         setNotes(data)
       })
-      .catch((error) => {
+      .catch(() => {
         setNotes([])
       })
   }, [])
 
-  return !_.isEmpty(notes) ? (
+  return (
     <div className="notes-container">
-      <Stack spacing={0}>
-        {notes && notes.map((note: INote) => <PrevNode note={note} />)}
-      </Stack>
-    </div>
-  ) : (
-    <div className="notes-container__no-notes-found">
-      <p className="notes-container__no-notes-message">
-        No se encontrarón notas
-      </p>
-      <Icon
-        className="notes-container__icon-sad-face"
-        style={{ color: 'rgba(255, 51, 102, 0.15)', transform: 'scale(0.8)' }}
+      {!_.isEmpty(notes) ? (
+        <Stack spacing={0}>
+          {notes && notes.map((note: INote) => <PrevNode note={note} />)}
+        </Stack>
+      ) : (
+        <div className="notes-container__no-notes-found">
+          <p className="notes-container__no-notes-message">
+            No se encontrarón notas
+          </p>
+          <Icon
+            className="notes-container__icon-sad-face"
+            style={{
+              color: 'rgba(255, 51, 102, 0.15)',
+              transform: 'scale(0.6)',
+            }}
+          />
+        </div>
+      )}
+      <SpeedDial
+        ariaLabel="Create new note"
+        sx={{ position: 'absolute' }}
+        icon={<SpeedDialIcon />}
       />
     </div>
   )
